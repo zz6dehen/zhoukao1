771375ec729442749c867722523581b74fecf70a
zhangzhe <289372329@qq.com>
2018-12-03 09:38:34 +0800

    zhangzhe

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@
  * @Author: 张哲 
  * @Date: 2018-12-03 09:25:07 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-03 09:29:05
+ * @Last Modified time: 2018-12-03 09:37:36
  */
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 var mincss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify')
 console.log(gulp)
 
 //起服务
@@ -25,7 +27,17 @@ gulp.task('sass', function() {
     return gulp.src('./src/scss/index.scss')
         .pipe(sass())
         .pipe(mincss())
-        .pipe(gulp.dest('./src/css'))
+        .pipe(gulp.dest('./build/css'))
 })
+
+//js编译压缩
+gulp.task('js', function() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('./build/scripts'))
+})
+
+
 
 // gulp.task('wacth')
